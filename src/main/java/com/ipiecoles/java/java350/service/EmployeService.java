4e4aac8ab25a2718a16a982af10fa54f4b157893
jayhem10 <noble.jka@gmail.com>
2021-02-09 14:42:37 +0100

    Ajout TU

--- src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
+++ src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
@@ -57,9 +57,8 @@ public class EmployeService {
         //Calcul du salaire
         Double salaire = Entreprise.COEFF_SALAIRE_ETUDES.get(niveauEtude) * Entreprise.SALAIRE_BASE;
         if(tempsPartiel != null){
-            salaire = salaire * tempsPartiel;
+            salaire = Math.round(salaire * tempsPartiel * 100) / 100d;
         }
-
         //Création et sauvegarde en BDD de l'employé.
         Employe employe = new Employe(nom, prenom, matricule, LocalDate.now(), salaire, Entreprise.PERFORMANCE_BASE, tempsPartiel);
 
