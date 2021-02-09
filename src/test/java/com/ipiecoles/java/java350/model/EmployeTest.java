51e8ca0dba1f78a5ff318d2e549b488e962f8b53
jayhem10 <noble.jka@gmail.com>
2021-02-09 09:40:44 +0100

    Ajout TU employeTest

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -93,24 +93,32 @@ public class EmployeTest {
 
     // Méthodes paramétrées
 
-    @ParameterizedTest(name = "Perf {0}, matricule {1}, tauxActivite {2}, anciennete {3},  => prime {4}")
+    @ParameterizedTest(name = "Perf {0}, matricule {1}, txActivite {2}, anciennete {3} => prime {4}")
     @CsvSource({
             "1, 'T12345', 1.0, 0, 1000.0",
             "1, 'T12345', 0.5, 0, 500.0",
             "2, 'T12345', 1.0, 0, 2300.0",
             "1, 'T12345', 1.0, 2, 1200.0",
-    })
-
-    // Données d'entrée qui peuvent varier => tempsPartiel, poste ( manager ou autre ), performance, dateEmbauche
-    void testGetPrimeAnnuelleWithParams(Integer performance, String matricule, Double tauxActivite, long nbAnneesAnciennete, Double primeAttendue) {
-        //Given, When, Then
-        Employe employe = new Employe("Doe", "John", matricule, LocalDate.now().minusYears(nbAnneesAnciennete),1500d, performance, tauxActivite);
-
+            })
+    public void testGetPrimeAnnuelle(Integer performance, String matricule, Double tauxActivite, Long nbAnneesAnciennete,
+                                     Double primeAttendue){
+        //Given
+        Employe employe = new Employe("Doe", "John", matricule,
+                LocalDate.now().minusYears(nbAnneesAnciennete), 1500d,
+                performance, tauxActivite);
         //When
         Double prime = employe.getPrimeAnnuelle();
         //Then
-
         Assertions.assertThat(prime).isEqualTo(primeAttendue);
     }
 
+    @Test
+    public void testGetPrimeAnnuelleMatriculeNull(){
+        //Given
+        Employe employe = new Employe("Doe", "John", null, LocalDate.now(), 1500d, 1, 1.0);
+        //When
+        Double prime = employe.getPrimeAnnuelle();
+        //Then
+        Assertions.assertThat(prime).isEqualTo(1000.0);
+    }
 }
