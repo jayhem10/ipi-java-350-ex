7cf5b25f15b34fa09c838165765838a2d6f24a66
jayhem10 <noble.jka@gmail.com>
2021-02-09 14:08:37 +0100

    Ajout TU

---
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
@@ -0,0 +1,27 @@
+package com.ipiecoles.java.java350.service;
+
+import com.ipiecoles.java.java350.repository.EmployeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeServiceTest {
+
+    @Autowired
+    private EmployeService employeService;
+
+    @Autowired
+    private EmployeRepository employeRepository;
+
+    public void test(){
+        //Given
+
+        //When
+        employeService.embaucheEmploye();
+
+        //Then
+
+
+    }
+
+}
