620ae8d6ba64d4fd776b8b52c6816175f2e3f4d1
jayhem10 <noble.jka@gmail.com>
2021-02-10 09:34:15 +0100

    test avec mock

--- src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
@@ -8,44 +8,100 @@ import com.ipiecoles.java.java350.repository.EmployeRepository;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.persistence.EntityExistsException;
 import java.time.LocalDate;
 import java.util.List;
 
-@SpringBootTest
+
+@ExtendWith(MockitoExtension.class)
 class EmployeServiceTest {
 
-    @Autowired
+    @InjectMocks
     private EmployeService employeService;
-
-    @Autowired
+    @Mock
     private EmployeRepository employeRepository;
 
     @Test
     public void testEmbauchePremierEmploye() throws EmployeException {
-        //Given
+        //Given Pas d'employés en base
         String nom = "Doe";
         String prenom = "John";
         Poste poste = Poste.TECHNICIEN;
         NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
         Double tempsPartiel = 1.0;
-
+        //Simuler qu'aucun employé n'est présent (ou du moins aucun matricule)
+        Mockito.when(employeRepository.findLastMatricule()).thenReturn(null);
+        //Simuler que la recherche par matricule ne renvoie pas de résultats
+        Mockito.when(employeRepository.findByMatricule("T00001")).thenReturn(null);
         //When
         employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel);
-
         //Then
-        List<Employe> employes = employeRepository.findAll();
-        Assertions.assertThat(employes).hasSize(1);
-        Employe employe = employeRepository.findAll().get(0);
+        ArgumentCaptor<Employe> employeArgumentCaptor = ArgumentCaptor.forClass(Employe.class);
+//        Mockito.verify(employeRepository, Mockito.times(1)).save(employeArgumentCaptor.capture());
+        Mockito.verify(employeRepository).save(employeArgumentCaptor.capture());
+        Employe employe = employeArgumentCaptor.getValue();
+        Assertions.assertThat(employe).isNotNull();
         Assertions.assertThat(employe.getNom()).isEqualTo(nom);
         Assertions.assertThat(employe.getPrenom()).isEqualTo(prenom);
         Assertions.assertThat(employe.getSalaire()).isEqualTo(1825.46);
         Assertions.assertThat(employe.getTempsPartiel()).isEqualTo(1.0);
         Assertions.assertThat(employe.getDateEmbauche()).isEqualTo(LocalDate.now());
         Assertions.assertThat(employe.getMatricule()).isEqualTo("T00001");
+    }
+
+    @Test
+    public void testEmbaucheLimiteMatricule() {
+        //Given Pas d'employés en base
+        String nom = "Doe";
+        String prenom = "John";
+        Poste poste = Poste.TECHNICIEN;
+        NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
+        Double tempsPartiel = 1.0;
+        //Simuler qu'il y a 99999 employés en base (ou du moins que le matricule le plus haut
+        //est X99999
+        Mockito.when(employeRepository.findLastMatricule()).thenReturn("99999");
+        //When
+        try {
+            employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel);
+            Assertions.fail("embaucheEmploye aurait dû lancer une exception");
+        } catch (EmployeException e) {
+            //Then
+            Assertions.assertThat(e.getMessage()).isEqualTo("Limite des 100000 matricules atteinte !");
+        }
 
     }
 
+    @Test
+    public void testEmbaucheEmployeExisteDeja() throws EmployeException {
+        //Given Pas d'employés en base
+        String nom = "Doe";
+        String prenom = "John";
+        Poste poste = Poste.TECHNICIEN;
+        NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
+        Double tempsPartiel = 1.0;
+        Employe employeExistant = new Employe("Doe", "Jane", "T00001", LocalDate.now(), 1500d, 1, 1.0);
+        //Simuler qu'aucun employé n'est présent (ou du moins aucun matricule)
+        Mockito.when(employeRepository.findLastMatricule()).thenReturn(null);
+        //Simuler que la recherche par matricule renvoie un employé (un employé a été embauché entre temps)
+        Mockito.when(employeRepository.findByMatricule("T00001")).thenReturn(employeExistant);
+        //When
+        try {
+            employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel);
+            Assertions.fail("embaucheEmploye aurait dû lancer une exception");
+        }catch (Exception e){
+            //Then
+            Assertions.assertThat(e).isInstanceOf(EntityExistsException.class);
+            Assertions.assertThat(e.getMessage()).isEqualTo("L'employé de matricule T00001 existe déjà en BDD");
+        }
+    }
+
 }
+
+
