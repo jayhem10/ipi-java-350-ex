49ee5f4f0819d61c3abe862b774c4bde7ba17677
jayhem10 <noble.jka@gmail.com>
2021-02-22 14:49:22 +0100

    code smells fix v3

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -20,7 +20,7 @@ class EmployeTest {
         //When
         Integer anneeAnciennete = employe.getNombreAnneeAnciennete();
         //Then
-        Assertions.assertThat(anneeAnciennete).isEqualTo(0);
+        Assertions.assertThat(anneeAnciennete).isNull();
     }
 
     @Test
